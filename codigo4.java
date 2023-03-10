300265c9a2f0202962e46fc64397f2cbd856f0f3
gabi-tarifa <126773354+gabi-tarifa@users.noreply.github.com>
2023-03-10 09:54:11 -0300

    Update codigo4.java

--- codigo4.java
+++ codigo4.java
@@ -2,12 +2,49 @@ import java.util.Scanner;
 
 public class codigo4 {
     public static int p1 = 0, p2 = 0;
+    public static int res = 0;
     public static int games = 1;
-    public static char[][] velha = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' }};
+    public static char[][] velha = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
     public static Scanner ler = new Scanner(System.in);
     public static int p1a = p1, p2a = p2;
 
-    public static int victory(int p1, p2) {
+    public static void main(String[] args) {
+        boolean keep_Going = true;
+        char keep = ' ';
+        int i = 0;
+        int x = 0, y = 0;
+        do {
+            for (i = 0; i < 9; i++) {
+                if (p1a != p1 || p2a != p2)
+                    break;
+                p1a = p1;
+                p2a = p2;
+                System.out.println(velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2]);
+                System.out.println("--|---|--");
+                System.out.println(velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2]);
+                System.out.println("--|---|--");
+                System.out.println(velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2]);
+                System.out.printf("Insira um numero de 0 a 2: ");
+                x = ler.nextInt();
+                System.out.printf("Insira outro numero de 0 a 2: ");
+                y = ler.nextInt();
+
+                if (games % 2 == 1)
+                    velha[x][y] = 'x';
+                else
+                    velha[x][y] = 'o';
+                victory(p1, p2);
+                games++;
+
+            }
+            System.out.printf("Continue? Press any key to stop ");
+            keep = ler.next().charAt(0);
+            if (keep != ' ')
+                keep_Going = false;
+        } while (keep_Going == true);
+    }
+
+    public static int victory(int res) {
         if (velha[0][0] == 'x' && velha[0][1] == 'x' && velha[0][2] == 'x') {
             p1++;
         } else if (velha[1][0] == 'x' && velha[1][1] == 'x' && velha[1][2] == 'x') {
@@ -41,40 +78,11 @@ public class codigo4 {
         } else if (velha[0][2] == 'x' && velha[1][1] == 'x' && velha[2][0] == 'x') {
             p1++;
         }
+        if (p1 < p2)
+            res = p2;
+        else if (p2 < p1)
+            res = p1;
+        return res;
     }
-    public static void main(String[] args) {
-        boolean keep_Going = true;
-        char keep = ' ';
-        int i = 0;
-        int x = 0, y = 0;
-        do {
-            for (i=0;i<9;i++) {
-                if (p1a != p1 || p2a != p2)
-                    break;
-                p1a = p1; p2a = p2;                
-                System.out.println(velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2]);
-                System.out.println("--|---|--");
-                System.out.println(velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2]);
-                System.out.println("--|---|--");
-                System.out.println(velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2]);
-                System.out.printf("Insira um numero de 0 a 2: ");
-                x = ler.nextInt();
-                System.out.printf("Insira outro numero de 0 a 2: ");
-                y = ler.nextInt();
 
-                if (games%2==1)
-                    velha[x][y] = 'x';
-                else
-                    velha[x][y] = 'o';
-                victory(p1, p2);
-                games++;
-                
-
-        }
-        System.out.printf("Continue? Press any key to stop ");
-        keep = ler.next().charAt(0);
-        if (keep != ' ')
-            keep_Going = false;
-    } while (keep_Going == true);
-    }
 }
