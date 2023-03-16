4c464a67530ea0eece624b33e210af457d170100
gabi-tarifa <126773354+gabi-tarifa@users.noreply.github.com>
2023-03-16 08:20:59 -0300

    Update codigo4.java

--- codigo4.java
+++ codigo4.java
@@ -15,32 +15,61 @@ public class codigo4 {
         int x = 0, y = 0;
         do {
             for (i = 0; i < 9; i++) {
-                if (p1a != p1 || p2a != p2)
-                    break;
-                p1a = p1;
-                p2a = p2;
                 System.out.println(velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2]);
                 System.out.println("--|---|--");
                 System.out.println(velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2]);
                 System.out.println("--|---|--");
                 System.out.println(velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2]);
+                System.out.println(" X " + p1 + "/" + p2 + " O");
+                if (p1a != p1 || p2a != p2)
+                    break;
+                p1a = p1;
+                p2a = p2;
                 System.out.printf("Insira um numero de 0 a 2: ");
                 x = ler.nextInt();
                 System.out.printf("Insira outro numero de 0 a 2: ");
                 y = ler.nextInt();
 
-                if (games % 2 == 1)
-                    velha[x][y] = 'x';
-                else
-                    velha[x][y] = 'o';
-                victory(p1, p2);
-                games++;
-
+                if(x != 0 && x != 1 && x != 2 && y != 0 && y != 1 && y != 2)
+                    System.out.println("Por favor insira um valor válido...");
+                else {
+                    if(velha[x][y] == ' ') {
+                        if (games % 2 == 1)
+                            velha[x][y] = 'x';
+                        else
+                            velha[x][y] = 'o';
+                    } else {
+                        System.out.println("Por favor insira outro par ordenado...");
+                        i = i + 1;
+                        games = games - 1;
+                    }
+                }
+            victory(res);
+            games++;
             }
-            System.out.printf("Continue? Press any key to stop ");
+            System.out.println(velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2]);
+            System.out.println("--|---|--");
+            System.out.println(velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2]);
+            System.out.println("--|---|--");
+            System.out.println(velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2]);
+            System.out.println(" X " + p1 + "/" + p2 + " O");
+            System.out.printf("Continue? Press any key to continue and 'n' to stop: ");
             keep = ler.next().charAt(0);
-            if (keep != ' ')
+            if (keep == 'n')
                 keep_Going = false;
+            else {
+                velha[0][0] = ' ';
+                velha[0][1] = ' ';
+                velha[0][2] = ' ';
+                velha[1][0] = ' ';
+                velha[1][1] = ' ';
+                velha[1][2] = ' ';
+                velha[2][0] = ' ';
+                velha[2][1] = ' ';
+                velha[2][2] = ' ';
+                p1a = p1;
+                p2a = p2;
+            }
         } while (keep_Going == true);
     }
 
